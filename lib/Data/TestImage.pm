3e64ca7eed91bf34fa06d455951bd10bce7a49b2
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-05-22 10:02:50 -0500

    add alternative URLs

--- lib/Data/TestImage.pm
+++ lib/Data/TestImage.pm
@@ -10,25 +10,25 @@ use constant USC_SIPI_IMAGE_DB_VOLUME => {
 			volume => 1,
 			description => 'Brodatz textures, texture mosaics, etc.',
 			url => 'http://sipi.usc.edu/database/textures.tar.gz',
-			alt_urls => [ '' ], # TODO
+			alt_urls => [ 'https://github.com/zmughal/usc-sipi-image-database-backup/blob/master/textures.tar.gz?raw=true' ],
 		},
 	aerials => {
 			volume => 2,
 			description => 'High altitude aerial images',
 			url => 'http://sipi.usc.edu/database/aerials.tar.gz',
-			alt_urls => [ '' ], # TODO
+			alt_urls => [ 'https://github.com/zmughal/usc-sipi-image-database-backup/blob/master/aerials.tar.gz?raw=true' ],
 		},
 	miscellaneous => {
 			volume => 3,
 			description => 'Lena, the mandrill, and other favorites',
 			url => 'http://sipi.usc.edu/database/misc.tar.gz',
-			alt_urls => [ '' ], # TODO
+			alt_urls => [ 'https://github.com/zmughal/usc-sipi-image-database-backup/blob/master/misc.tar.gz?raw=true' ],
 		},
 	sequences => {
 			volume => 4,
 			description => 'Moving head, fly-overs, moving vehicles',
 			url => 'http://sipi.usc.edu/database/sequences.tar.gz',
-			alt_urls => [ '' ], # TODO
+			alt_urls => [ 'https://github.com/zmughal/usc-sipi-image-database-backup/blob/master/sequences.tar.gz?raw=true' ],
 		},
 };
 
